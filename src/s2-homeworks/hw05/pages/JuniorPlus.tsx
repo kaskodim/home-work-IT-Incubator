a60dc3e45aa9cec181c14b1c88b4a5f040aed7c0
DmitriyGulevich <kaskodim@gmail.com>
2025-06-23 17:14:01 +0300

    HW15.1

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -10,11 +10,11 @@ import HW15 from '../../hw15/HW15';
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
-            {/*<HW10 />*/}
-            {/*<HW11 />*/}
-            {/*<HW12 />*/}
-            {/*<HW13 />*/}
-            {/*<HW14 />*/}
+            <HW10 />
+            <HW11 />
+            <HW12 />
+            <HW13 />
+            <HW14 />
             <HW15 />
         </div>
     )
